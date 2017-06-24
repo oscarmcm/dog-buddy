67aa8ca93859e2bc7c065075eb5502da400f890a
Darwing Medina <darwingjavier31@yahoo.es>
2017-06-24 11:37:04 -0600

    DB-2 Initial markup for sign up form

--- pages/Login.js
+++ pages/Login.js
@@ -6,6 +6,7 @@ import {StyleSheet, View, Text} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import Button from 'apsl-react-native-button'
 import * as Animatable from 'react-native-animatable';
+import { GiftedForm, GiftedFormManager } from 'react-native-gifted-form'
 
 import BackgroundImage from '../components/BackgroundImage'
 
@@ -36,20 +37,119 @@ export default class Login extends Component {
   render() {
     return (
       <BackgroundImage path={require('../images/login_bck.jpg')}>
+        <View style={styles.container}>
+          <Button onPress={this.onSignIn.bind(this)} style={styles.button} textStyle={styles.text}>Sign In</Button>
+          <Button onPress={this.onSignUp.bind(this)} style={styles.button} textStyle={styles.text}>Sign Up</Button>
+        </View>
         {this.state.showSignIn ?
           <Animatable.View animation="fadeInUpBig" duration={400} useNativeDriver style={styles.loginForm}>
+            <Button onPress={this.onSignIn.bind(this)} style={styles.closeButton} textStyle={{color: 'white'}}>X</Button>
             <Text>Aqui va el login form</Text>
           </Animatable.View>
         : null}
         {this.state.showSignUp ?
           <Animatable.View animation="fadeInUpBig" duration={400} useNativeDriver style={styles.loginForm}>
-            <Text>Aqui va el registro form</Text>
+            <Button onPress={this.onSignUp.bind(this)} style={styles.closeButton} textStyle={{color: 'white'}}>X</Button>
+            <GiftedForm
+              formName='signupForm'
+              openModal={(route) => {
+                navigator.push(route);
+              }}
+              clearOnClose={true}
+              validators={{
+                emailAddress: {
+                  title: 'Email address',
+                  validate: [{
+                      validator: 'isLength',
+                      arguments: [6, 255],
+                    }, 
+                    { validator: 'isEmail', }
+                  ]
+                },
+                username: {
+                  title: 'Username',
+                  validate: [{
+                    validator: 'isLength',
+                    arguments: [3, 16],
+                    message: '{TITLE} must be between {ARGS[0]} and {ARGS[1]} characters'
+                  },{
+                    validator: 'matches',
+                    arguments: /^[a-zA-Z0-9]*$/,
+                    message: '{TITLE} can contains only alphanumeric characters'
+                  }]
+                },
+                password: {
+                  title: 'Password',
+                  validate: [{
+                    validator: 'isLength',
+                    arguments: [6, 16],
+                    message: '{TITLE} must be between {ARGS[0]} and {ARGS[1]} characters'
+                  }]
+                },
+              }}
+            >
+              <GiftedForm.SeparatorWidget />
+
+              <GiftedForm.TextInputWidget
+                name='emailAddress' // mandatory
+                title='Email address'
+                placeholder='example@nomads.ly'
+                keyboardType='email-address'
+                clearButtonMode='while-editing'
+                // image={require('../../assets/icons/color/email.png')}
+              />
+
+              <GiftedForm.TextInputWidget
+                name='username'
+                title='Username'
+                // image={require('../../assets/icons/color/contact_card.png')}
+                placeholder='Your username'
+                clearButtonMode='while-editing'
+                onTextInputFocus={(currentText = '') => {
+                  return currentText;
+                }}
+              />
+
+              <GiftedForm.TextInputWidget
+                name='password' // mandatory
+                title='Password'
+                placeholder='******'
+                clearButtonMode='while-editing'
+                secureTextEntry={true}
+                // image={require('../../assets/icons/color/lock.png')}
+              />
+
+              <GiftedForm.SeparatorWidget />
+
+              <GiftedForm.ErrorsWidget/>
+
+              <GiftedForm.SubmitWidget
+                title='Sign up'
+                widgetStyles={{
+                  submitButton: {
+                    backgroundColor: '#1ABC9C',
+                  }
+                }}
+                onSubmit={(isValid, values, validationResults, postSubmit = null, modalNavigator = null) => {
+                  if (isValid === true) {
+                    /* Implement the request to your server using values variable
+                    ** then you can do:
+                    ** postSubmit(); // disable the loader
+                    ** postSubmit(['An error occurred, please try again']); // disable the loader and display an error message
+                    ** postSubmit(['Username already taken', 'Email already taken']); // disable the loader and display an error message
+                    ** GiftedFormManager.reset('signupForm'); // clear the states of the form manually. 'signupForm' is the formName used
+                    */
+                  }
+                }}
+              />
+
+              <GiftedForm.NoticeWidget
+                title='By signing up, you agree to the Terms of Service and Privacy Policity.'
+              />
+              <GiftedForm.HiddenWidget name='tos' value={true} />
+            </GiftedForm>
           </Animatable.View>
         : null}
-        <View style={styles.container}>
-          <Button onPress={this.onSignIn.bind(this)} style={styles.button} textStyle={styles.text}>Sign In</Button>
-          <Button onPress={this.onSignUp.bind(this)} style={styles.button} textStyle={styles.text}>Sign Up</Button>
-        </View>
       </BackgroundImage>
     )
   }
@@ -78,6 +178,17 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
     marginBottom: 0,
     height: 45,
+  },
+  closeButton: {
+    backgroundColor: '#3b4045',
+    borderColor: '#3b4045',
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+    position: 'absolute',
+    top: -25,
+    right: 15,
+    zIndex: 10,
   },
   text: {
     color: 'black',
